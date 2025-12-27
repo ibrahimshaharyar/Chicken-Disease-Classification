1fe51e6fe2fd0e49ce8cb7a9713831ad01b4c479
ibrahimshaharyar <ibrahimshaharyar41@gmail.com>
2025-12-28 02:37:11 +0500

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from CNN_based_disease_classifier import logger
 from CNN_based_disease_classifier.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
+from CNN_based_disease_classifier.pipeline.step_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion Step"
@@ -13,3 +15,15 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
