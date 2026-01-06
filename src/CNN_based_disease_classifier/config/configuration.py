89ad25b530f1d6dd4bbed0814d135e63433d6f5e
ibrahimshaharyar <ibrahimshaharyar41@gmail.com>
2026-01-06 22:51:20 +0500

    prepared callbacks

--- src/CNN_based_disease_classifier/config/configuration.py
+++ src/CNN_based_disease_classifier/config/configuration.py
@@ -1,6 +1,8 @@
 from CNN_based_disease_classifier.constants import *
 from CNN_based_disease_classifier.utils.common import read_yaml, create_directories
-from CNN_based_disease_classifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig)
+import os
+from CNN_based_disease_classifier.entity.config_entity import (DataIngestionConfig, PrepareBaseModelConfig, 
+                                                               PrepareCallbacksConfig)
 
 
 class ConfigurationManager:
@@ -44,3 +46,20 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+
+    def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+        config = self.config.prepare_callbacks
+        
+        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+        create_directories([
+            Path(model_ckpt_dir),
+            Path(config.tensorboard_root_log_dir)
+        ])
+        
+        prepare_callback_config = PrepareCallbacksConfig(
+            root_dir= Path(config.root_dir),
+            tensorboard_root_log_dir= Path(config.tensorboard_root_log_dir),
+            checkpoint_model_filepath= Path(config.checkpoint_model_filepath)
+        )
+        
+        return prepare_callback_config
